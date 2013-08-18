c0e5ca633bf0e1ff95ef5376f33f36b9345d8687
Ayumi Nishi <naym24@gmail.com>
2013-08-18 15:32:29 +0900

    new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/filter_parameter_logging.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/initializers/wrap_parameters.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/migrate/20130816040240_create_products.rb 	new file:   db/migrate/20130816071739_create_carts.rb 	new file:   db/migrate/20130816073214_create_line_items.rb 	new file:   db/migrate/20130816105537_add_quantity_to_line_items.rb 	new file:   db/migrate/20130816121523_combine_items_in_cart.rb 	new file:   db/schema.rb 	new file:   db/seeds.rb
    	lib/
    	log/
    	public/
    	test/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '0761474136c1d5432b56e972fdbc45cbc79a72a955b040faa6958232b4b6e6621bb8c1ba3cb2a317a1b3d7c2ab22a28a4bb7ebc261f4d6a27f4c367e44e285ca'
